d42f2bc773277598a65e0d89de93510ca3300d2c
Jay Chen <sm00000660@gmail.com>
2020-09-02 14:34:58 +0800

    Project init

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { restaurant: restaurantList.results });
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurant = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase());
+  });
+  res.render("index", { restaurant: restaurant, keyword: keyword });
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`);
+});
