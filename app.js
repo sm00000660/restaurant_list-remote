aa793e69a8526edddb19edf0bd2797640391bdf7
Jay Chen <sm00000660@gmail.com>
2020-09-06 20:15:23 +0800

    feat: add show page

--- app.js
+++ app.js
@@ -69,6 +69,22 @@ app.post("/restaurant", (req, res) => {
     .catch((error) => console.log(error));
 });
 
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render("show", { restaurant }))
+    .catch((error) => console.log(error));
+});
+
+// app.get("/restaurants/:restaurant_id", (req, res) => {
+//   const id = req.params.restaurant_id;
+//   Restaurant.findOne({ id: `${id}` })
+//     .lean()
+//     .then((restaurant) => res.render("show", { restaurant }))
+//     .catch((error) => console.log(error));
+// });
+
 // app.get("/restaurants/:restaurant_id", (req, res) => {
 //   const restaurant = restaurantList.results.find(
 //     (restaurant) => restaurant.id.toString() === req.params.restaurant_id
