db61c0c23cad64defa4a5d386fd8986dc14b3c83
Jay Chen <sm00000660@gmail.com>
2020-09-06 21:49:07 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -77,13 +77,48 @@ app.get("/restaurants/:id", (req, res) => {
     .catch((error) => console.log(error));
 });
 
-// app.get("/restaurants/:restaurant_id", (req, res) => {
-//   const id = req.params.restaurant_id;
-//   Restaurant.findOne({ id: `${id}` })
-//     .lean()
-//     .then((restaurant) => res.render("show", { restaurant }))
-//     .catch((error) => console.log(error));
-// });
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render("edit", { restaurant }))
+    .catch((error) => console.log(error));
+});
+
+app.post("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const name_en = req.body.name_en;
+  const category = req.body.category;
+  const image = req.body.image;
+  const location = req.body.location;
+  const phone = req.body.phone;
+  const rating = req.body.rating;
+  const google_map = req.body.google_map;
+  const description = req.body.description;
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name;
+      restaurant.name_en = name_en;
+      restaurant.category = category;
+      restaurant.image = image;
+      restaurant.location = location;
+      restaurant.phone = phone;
+      restaurant.rating = rating;
+      restaurant.description = description;
+      return restaurant.save();
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch((error) => console.log(error));
+});
+
+app.post("/restaurants/:id/delete", (req, res) => {
+  const id = req.params.id;
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
 // app.get("/restaurants/:restaurant_id", (req, res) => {
 //   const restaurant = restaurantList.results.find(
