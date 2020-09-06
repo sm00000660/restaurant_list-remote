6dab930331c7d8426559a38784efef65003cf7cf
Jay Chen <sm00000660@gmail.com>
2020-09-06 13:24:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 const exphbs = require("express-handlebars");
@@ -8,6 +9,21 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/restaurant-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 app.get("/", (req, res) => {
   res.render("index", { restaurant: restaurantList.results });
